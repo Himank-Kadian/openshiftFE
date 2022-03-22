5c224144c4db8fe0cab8e3fbf7c6ebe19ea26d4e
Himank <himank.kadian@cgi.com>
2022-03-22 10:02:24 -0700

    added index file

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+$servername = "openshift6";
+$username = "root";
+$password = "123";
+$dbname = "cgi";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT  itemName, qty FROM list";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "Item: " . $row["itemName"]. " " . $row["qty"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
